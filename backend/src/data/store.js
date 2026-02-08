9c1d103b2b3f4719b1cc56ed9d0871fd325590c7
Vassu627 <vasanthar627@gmail.com>
2026-02-08 15:45:17 +0530

    added more products in store

--- backend/src/data/store.js
+++ backend/src/data/store.js
@@ -3,13 +3,37 @@ export const products = [
     id: "1",
     name: "Gold Ring",
     price: 2500,
-    image: "https://via.placeholder.com/200",
+    image: "https://images.unsplash.com/photo-1605100804763-247f67b3557e",
   },
   {
     id: "2",
     name: "Diamond Necklace",
     price: 8500,
-    image: "https://via.placeholder.com/200",
+    image: "https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f",
+  },
+  {
+    id: "3",
+    name: "Silver Bracelet",
+    price: 1500,
+    image: "https://images.unsplash.com/photo-1611652022419-a9419f74343d",
+  },
+  {
+    id: "4",
+    name: "Pearl Earrings",
+    price: 3200,
+    image: "https://images.unsplash.com/photo-1588444837495-c6cfeb53b5a6",
+  },
+  {
+    id: "5",
+    name: "Rose Gold Pendant",
+    price: 4100,
+    image: "https://images.unsplash.com/photo-1617038260897-41a1f14a8ca0",
+  },
+  {
+    id: "6",
+    name: "Platinum Wedding Band",
+    price: 12000,
+    image: "https://images.unsplash.com/photo-1602751584552-8ba73aad10e1",
   },
 ];
 
